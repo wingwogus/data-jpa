2f3da4971c3e41112a69991dc875ab28216110dd
wingwogus <wingwogus@naver.com>
2025-02-14 23:52:36 +0900

    section 2

--- src/main/java/study/data_jpa/repository/MemberJpaRepository.java
+++ src/main/java/study/data_jpa/repository/MemberJpaRepository.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -18,6 +21,23 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(em.find(Member.class, id));
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
--- src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -17,14 +19,43 @@ class MemberJpaRepositoryTest {
     @Test
     public void testMember() throws Exception{
         //given
-////        Member member = new Member();
-//        memberJpaRepository.save(member);
-//        //when
-//        Member findMember = memberJpaRepository.find(member.getId());
-//
-//        //then
-//        assertThat(findMember.getId()).isEqualTo(member.getId());
-//        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Member member = new Member("KIM");
+        memberJpaRepository.save(member);
+        //when
+        Member findMember = memberJpaRepository.find(member.getId());
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.find(member1.getId());
+        Member findMember2 = memberJpaRepository.find(member2.getId());
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(findMember1);
+        memberJpaRepository.delete(findMember2);
+
+        long deleteCount = memberJpaRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
     }
 
 }
